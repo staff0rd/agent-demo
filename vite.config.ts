849de0dd299c79370376af1b9471c6b95848718a
Stafford Williams <staff0rd@users.noreply.github.com>
2025-01-08 20:53:24 +1100

    Update Vite config with webhook proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/webhook': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+    watch: {
+      // Watch for changes in the entire project directory
+      ignored: ['!**/node_modules/**'],
+    },
+  },
 })
