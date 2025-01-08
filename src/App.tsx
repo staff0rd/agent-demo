8070e61c34277db89eca527c705832a4384db71d
Stafford Williams <staff0rd@users.noreply.github.com>
2025-01-08 20:43:39 +1100

    Add basic Vite + React setup files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Agent Demo</h1>
+      <p>Welcome to the Agent Demo application!</p>
+    </div>
+  )
+}
+
+export default App
